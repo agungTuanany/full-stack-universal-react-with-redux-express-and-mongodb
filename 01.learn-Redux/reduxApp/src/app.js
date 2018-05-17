d3345f7547020234734814c35016e94b7dfcbe7c
agungTuanany <agung.tuanany@gmail.com>
2018-05-18 02:42:30 +0700

    Append the third book on the state by using state.concat()

--- 01.learn-Redux/reduxApp/src/app.js
+++ 01.learn-Redux/reduxApp/src/app.js
@@ -3,10 +3,11 @@
 import { createStore } from 'redux';
 
 // Step 3 define reduces
-const reducer = (state = {}, action) => {
+const reducer = (state = [], action) => {
     switch (action.type) {
         case "POST_BOOK":
-            return state = action.payload;
+        let books = state.concat(action.payload);
+            return books;
             break;
     }
     return state
@@ -17,16 +18,32 @@ const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log(`current state is:`, store.getState());
-    console.log(`current price:`, store.getState().price);
+    // console.log(`current price:`, store.getState()[1].price);
 });
 
 // step 2 create and dispatch action
 store.dispatch({
     type: "POST_BOOK",
-    payload: {
+    payload: [{
         id: 1,
         title: 'this is book title',
         description: 'this is the book description',
         price: 55.33
+    }, {
+        id: 2,
+        title: 'this is the second book title',
+        description: 'this is the second book description',
+        price: 66
+    }]
+})
+
+// DISPATCH a second time
+store.dispatch({
+    type: "POST_BOOK",
+    payload: {
+        id: 3,
+        title: 'this is the thrid book title',
+        description: 'this is the third book description',
+        price: 33
     }
 })
