535914eb4e89dffb937bb7bdc763b4fccc162a0c
agungTuanany <agung.tuanany@gmail.com>
2018-05-24 23:39:39 +0700

    Add a “...read more” button in bookItem component

--- 01.learn-Redux/reduxApp/src/components/pages/BookItem.js
+++ 01.learn-Redux/reduxApp/src/components/pages/BookItem.js
@@ -40,6 +40,16 @@ class BookItem extends Component {
         this.props.addToCart(book);
     }
 
+    constructor() {
+        super();
+        this.state = {
+            isClicked: false
+        };
+    }
+    onReadMore() {
+        this.setState({ isClicked: true });
+    }
+
     render() {
         return (
             <Well>
@@ -49,7 +59,22 @@ class BookItem extends Component {
                     </Col>
                     <Col xs={6} sm={8}>
                         <h6>{this.props.title}</h6>
-                        <p>{this.props.description}</p>
+                        <p>
+                            {this.props.description.length > 50 &&
+                            this.state.isClicked === false
+                                ? this.props.description.substring(0, 50)
+                                : this.props.description}
+                            <button
+                                className="link"
+                                onClick={this.onReadMore.bind(this)}
+                            >
+                                {this.state.isClicked === false &&
+                                this.props.description !== null &&
+                                this.props.description.length > 50
+                                    ? "...read more"
+                                    : ""}
+                            </button>
+                        </p>
                         <h6>usd. {this.props.price}</h6>
                         <Button
                             onClick={this.handleCart.bind(this)}
